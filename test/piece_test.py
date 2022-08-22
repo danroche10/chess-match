496ce495ed19f8b693cd01080ab43309022235f4
danroche10 <danielroche10@gmail.com>
2022-08-22 10:46:10 +0100

    Tests: added piece tests

---
+++ test/piece_test.py
@@ -0,0 +1,19 @@
+import pytest
+from chess.constants import BLUE
+from chess.piece import Piece
+
+@pytest.fixture
+def new_piece():
+  piece = Piece(6, 3, BLUE, "PAWN")
+  return piece
+
+def test_type(new_piece):
+    assert  new_piece.type == "PAWN"
+
+def test_move(new_piece):
+    new_piece.move(4, 3)
+    new_row = new_piece.row
+    new_col = new_piece.col
+    assert new_row == 4
+    assert new_col == 3
+
